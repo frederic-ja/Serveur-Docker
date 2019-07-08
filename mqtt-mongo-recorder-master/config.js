1b10b2e207c6eccd633619979bddab11b5db6e6d
Frederic JA <frederic.jenn.alet@gmail.com>
2019-07-08 07:33:43 +0000

    Change IP on Config.js

--- mqtt-mongo-recorder-master/config.js
+++ mqtt-mongo-recorder-master/config.js
@@ -4,11 +4,11 @@ config.debug = process.env.DEBUG || false;
 
 config.mqtt  = {};
 config.mqtt.namespace = process.env.MQTT_NAMESPACE || 'Pomo/#';
-config.mqtt.hostname  = process.env.MQTT_HOSTNAME  || '192.168.1.83';
+config.mqtt.hostname  = process.env.MQTT_HOSTNAME  || '192.168.1.71';
 config.mqtt.port      = process.env.MQTT_PORT      || 1883;
 
 config.mongodb = {};
-config.mongodb.hostname   = process.env.MONGODB_HOSTNAME   || '192.168.1.83';
+config.mongodb.hostname   = process.env.MONGODB_HOSTNAME   || '192.168.1.71';
 config.mongodb.port       = process.env.MONGODB_PORT       || 27017;
 config.mongodb.database   = process.env.MONGODB_DATABASE   || 'Pomo';
 config.mongodb.collection = process.env.MONGODB_COLLECTION || 'status';
